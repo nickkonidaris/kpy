6ac8d6eef05f33aa3a55bbd83dd8e6454d539ab4
nickkonidaris <nick.konidaris@gmail.com>
2015-07-30 09:13:51 -0700

    Add flat fielding based on Barlow single-number methods

--- SEDMr/Flat.py
+++ SEDMr/Flat.py
@@ -11,6 +11,7 @@ import NPK.Fit as FF
 import NPK.Bar as Bar
 from astropy.table import Table 
 
+
 from scipy.spatial import KDTree 
 import scipy.signal as SG
 from scipy.interpolate import interp1d
@@ -20,54 +21,63 @@ from numpy.polynomial.chebyshev import chebfit, chebval
 
 import SEDMr.Extraction as Extraction
 import SEDMr.Wavelength as Wavelength
+from SEDMr.FlatCorrection import FlatCorrection as FC
+
 reload(FF)
 reload(Extraction)
 reload(Wavelength)
 
 
-def measure_flat(fine, HDUlist, 
-        lamstart=1000,
-        lamratio=239./240.,
-        lamlen=250,
-        extract_width=3,
-        order=7,
+def measure_flat(extraction, meta, 
+        lamstart=700,
+        lamend=850,
         outfile='flat.npy'):
 
-    dat = HDUlist[0].data
 
-    lamgrid = Wavelength.fiducial_spectrum(lamstart=lamstart,
-        lamratio=lamratio, len=lamlen)
+    corrections = []
+    Xs = []
+    Ys = []
+    for i,e in enumerate(extraction):
+        fc = FC(seg_id=e.seg_id)
+        corrections.append(fc)
 
-    specgrid = np.zeros((len(lamgrid), len(fine)))
-    
-    for i,f in enumerate(fine):
+        if not e.ok: continue
 
-        if not f.ok: continue
-        if f.lamrms > 1: continue
-        if f.xrange[1] - f.xrange[0] < 200: continue
-
-        spec = np.zeros(f.xrange[1] - f.xrange[0])
-        yfun = np.poly1d(f.poly)
-
-        for jx,xpos in enumerate(np.arange(f.xrange[0], f.xrange[1])):
-            ypos = np.round(yfun(xpos))
-
-            try:spec[jx] = np.sum(dat[ypos-extract_width:ypos+extract_width,
-                    xpos])
-            except: continue
-
-        try:ll = chebval(np.arange(len(spec)), f.lamcoeff)
+        try: l,f = e.get_flambda()
         except: continue
-        specfun = interp1d(ll, spec, bounds_error=False)
-        specgrid[:,i] = specfun(lamgrid)
-            
-    flatspec = np.median(specgrid, axis=1)
 
-    chebs = np.zeros((order+1, len(fine)))
-    for i in np.arange(len(fine)):
-        chebs[:,i] = chebfit(lamgrid, specgrid[:,i] / flatspec, order)
+        X = np.argmin(np.abs(l-656))
+        Xs.append(e.xrange[0] + X)
+        Ys.append(np.mean(e.yrange))
 
-    return chebs
+        ROI = (l>lamstart) & (l <= lamend)
+        fc.correction = np.nanmean(f[ROI])
+
+    vals = [f.get_correction(0) for f in corrections]
+    medval = np.median(vals)
+
+    Ss = []
+    for c in corrections: 
+        try: 
+            c.correction /= medval
+            if c.correction > 2:
+                c.correction = 1.0
+            Ss.append(c.correction)
+        except: pass
+    
+    pl.figure(1)
+    pl.clf()
+    pl.scatter(Xs, Ys, c=Ss,s=70,linewidth=0, vmin=0.8,vmax=1.2,marker='h')
+    pl.xlim(-100,2048+100)
+    pl.ylim(-100,2048+100)
+    pl.colorbar()
+    pl.xlabel("X pixel")
+    pl.ylabel("Y pixel")
+    pl.title("Single correction from %s to %s from %s" % (lamstart, lamend,
+        meta['outname']))
+    pl.savefig("flat-field-values.pdf")
+
+    return corrections
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=\
@@ -76,25 +86,16 @@ if __name__ == '__main__':
         ''', formatter_class=argparse.RawTextHelpFormatter)
 
 
-    parser.add_argument('fine', type=str, help='Fine correction path')
     parser.add_argument('infile', type=str, help='Path to dome flat')
-    parser.add_argument('--lamstart', type=float, help='Wavelength resolution for interpolating grid', default=1000.0)
-    parser.add_argument('--lamratio', type=float, help='Wavelength resolution for interpolating grid', default=239.0/240.0)
-    parser.add_argument('--lamlen', type=int, help='Wavelength grid length', default=250)
-    parser.add_argument('--extract_width', type=int, help='Extraction width for spectrum in the Y direction (for X flexure)', default=3)
-    parser.add_argument('--order', type=int, help='Chebyshev polynomial order', default=7)
-    parser.add_argument('--outfile', type=str, help='Output filename', default="flexure.npy")
+    parser.add_argument('--lamstart', type=float, help='Wavelength resolution for interpolating grid', default=700.0)
+    parser.add_argument('--lamend', type=float, help='Wavelength resolution for interpolating grid', default=900.0)
+    parser.add_argument('--outfile', type=str, help='Output filename', default="flat-dome-700to900.npy")
 
     args = parser.parse_args()
 
-    fine = np.load(args.fine)
-    HDU = pf.open(args.infile)
-    flat = measure_flat(fine, HDU, 
+    ext, meta = np.load(args.infile)
+    flat = measure_flat(ext, meta,
         lamstart=args.lamstart,
-        lamratio=args.lamratio,
-        lamlen=args.lamlen,
-        extract_width=args.extract_width,
-        order=args.order,
-        outfile=args.outfile)
+        lamend=args.lamend)
 
     np.save(args.outfile, flat)
